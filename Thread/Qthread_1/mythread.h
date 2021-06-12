3f24304afdf002ae39b25599828718f12c443dfc
Sakura1221 <1442182454@qq.com>
2021-06-12 23:55:08 +0800

    Thread

---
+++ Thread/Qthread_1/mythread.h
@@ -0,0 +1,16 @@
+#ifndef MYTHREAD_H
+#define MYTHREAD_H
+
+#include <QObject>
+
+class MyThread : public QObject
+{
+    Q_OBJECT
+public:
+    explicit MyThread(QObject *parent = nullptr);
+
+signals:
+
+};
+
+#endif // MYTHREAD_H
